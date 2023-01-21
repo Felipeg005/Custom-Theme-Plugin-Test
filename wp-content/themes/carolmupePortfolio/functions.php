3448cbb06fc757add0e3677496f5e38b1f0b068d
Felipeg005 <juanfelipegomeztobon@gmail.com>
2023-01-21 10:29:04 -0500

    Add function documentation and dynamic language on header

--- wp-content/themes/carolmupePortfolio/functions.php
+++ wp-content/themes/carolmupePortfolio/functions.php
@@ -1,9 +1,21 @@
-<?php 
+<?php
 
+/**
+ * Carolmupe Add Scripts Function
+ * 
+ * Adds all needed scripts and styles on the head and footer injected by WordPress
+ * 
+ * @package carolmupePortfolio
+ * 
+ * @return void
+ */
 function carolmupe_add_scripts() {
+    // Styles
     wp_enqueue_style( 'style',  get_stylesheet_uri(), array(), wp_get_theme()->get('Version') , 'all' );
-    wp_enqueue_style( 'primary-styles',  get_template_directory_uri() . '/assets/css/primary-styles.css', array('secondary-styles'), wp_get_theme()->get('Version'), 'all' );
-    wp_enqueue_style('secondary-styles', get_template_directory_uri() . '/assets/css/secondary-styles.css', array(  ), wp_get_theme()->get('Version'), 'all' );
+
+
+    // Scripts
+    wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/custom.js', array(), wp_get_theme()->get('Version'), true);
 }
 
 add_action( 'wp_enqueue_scripts', 'carolmupe_add_scripts' );
--- wp-content/themes/carolmupePortfolio/header.php
+++ wp-content/themes/carolmupePortfolio/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 <head>
     <?php wp_head(); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carol Mupe Designer</title>
+    <title><?php echo get_the_title(); ?></title>
 </head>
 <body>
